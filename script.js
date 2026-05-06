416a9bb9568aa90370dc75ba2298694101f13672
Gadfi-almayasiy <almayasiy@gmail.com>
2026-05-06 23:11:23 +0100

    Retrieve code from URL parameters

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const BASE_URL = "https://xetmneyuecjqenskvgba.supabase.co/rest/v1/membres";
 const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldG1uZXl1ZWNqcWVuc2t2Z2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0OTU0NzcsImV4cCI6MjA5MzA3MTQ3N30.EKrp13-dZEmXGR_WDZjaBdRtmCvYM35pi1Wb6rXMa1Y";
 
-const code = "CSENA-2026-02";
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
 
 fetch(`${BASE_URL}?code_membre=eq.${code}`, {
   headers: {
